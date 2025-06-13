f5db15d10df578f2af4644fe951f6e34a4c2314c
aslankalpe <aslankalpe@Aslans-MacBook-Pro-2.local>
2025-06-14 01:42:00 +0500

    refactor: remove unused imports and timer functionality from Content component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,32 +1,9 @@
-import { useEffect } from 'react';
-
 import { Paper } from '@ozen-ui/kit/Paper';
-import { useSnackbar } from '@ozen-ui/kit/Snackbar';
-import { useTimer } from '@ozen-ui/kit/useTimer';
-
-import { version } from '../../../node_modules/@ozen-ui/kit/package.json';
 
 import { Header, Main } from './components';
 import s from './Content.module.css';
 
 export const Content = () => {
-  const { pushMessage } = useSnackbar();
-
-  const { startTimer } = useTimer({
-    startTime: 0,
-    endTime: 2000,
-    onTimerEnd: () => {
-      pushMessage({
-        title: 'Демо-стенд библиотеки Ozen-UI',
-        description: `Версия: ${version}`,
-        status: 'success',
-      });
-    },
-  });
-
-  useEffect(() => {
-    startTimer();
-  }, [startTimer]);
 
   return (
     <Paper className={s.content} radius="l">
